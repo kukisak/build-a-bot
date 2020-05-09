4ad983cbe10e33db2e3cbbdceb87d25f462d3199
Petr Kukrál <kukral.petr@ifortuna.cz>
2020-05-09 13:44:51 +0200

    7-3: added arg and modifiers for custom directive

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,5 +1,6 @@
 export default {
-  bind: (element) => {
+  bind: (element, binding) => {
+    console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
     element.style.position = 'absolute';
     element.style.bottom = '5px';
     element.style.right = '5px';
