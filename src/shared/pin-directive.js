6a33995b59e7625a101722615372b574213b3f35
Petr Kukrál <kukral.petr@ifortuna.cz>
2020-05-09 14:20:57 +0200

    7-3: added hook bind: and update: together with simple use; removed useless display of pinPadding value

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,18 +1,6 @@
-function applyStyle(element, binding) {
+export default function (element, binding) { // this way we add both hooks bind: and update:
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
-
-export default {
-  bind: (element, binding) => {
-    applyStyle(element, binding); // bind and update are typically used together to apply styles
-  },
-  update: (element, binding) => {
-    applyStyle(element, binding);
-  },
-  inserted: {}, // hook when element is inserted into parent element
-  componentUpdated: {}, // hook fires when all component childred have been updated
-  unbind: {}, // hook fires when directive is unbound from its parent component
-};
