8ee102e0235f291b7e65f694b3aa23334b7e1fea
Petr Kukrál <kukral.petr@ifortuna.cz>
2020-05-09 14:15:54 +0200

    7-3: added other hooks to demonstrate condition when they are fired

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -7,9 +7,12 @@ function applyStyle(element, binding) {
 
 export default {
   bind: (element, binding) => {
-    applyStyle(element, binding);
+    applyStyle(element, binding); // bind and update are typically used together to apply styles
   },
   update: (element, binding) => {
     applyStyle(element, binding);
   },
+  inserted: {}, // hook when element is inserted into parent element
+  componentUpdated: {}, // hook fires when all component childred have been updated
+  unbind: {}, // hook fires when directive is unbound from its parent component
 };
